23d7e1257b25d921f1b5b1f593df94b5f8ee6d3e
ryt <rytubuntulinux@sina.com>
2015-04-08 08:50:39 +0800

    2 second

---
+++ sum.cxx
@@ -0,0 +1,12 @@
+//A sum program
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	float a,b;
+	cout<<"请输入要相加的两个数,并以空格分开:";
+	cin>>a>>b;
+	cout<<"结果:"<<a+b<<endl;
+	return 0;
+}
---
+++ sum.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+print("**********")
+a=input("imput 1:")
+b=input("imput 2:")
+print("result:%f"%(a+b))
