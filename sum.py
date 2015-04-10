2d9cf15a1b852b632504494a5ed79222e0e0860d
ryt <rytubuntulinux@sina.com>
2015-04-10 14:26:24 +0800

    commit at 2015-4-10 14:24:00

--- sum.py
+++ sum.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
 
-print("**********")
-a=input("imput 1:")
-b=input("imput 2:")
+print("*****This is sum program,please input two digit*****")
+a=input("input first digit:")
+a=str(a)
+while not a.isdigit():
+	print("The input is not invalid!Please input a digit")
+	a=input("input first digit:")
+a=int(a)
+b=input("input second digit:")
+b=str(b)
+while not b.isdigit():
+	print("The input is not invalid!Please input a digit")
+	b=input("input the second digit:")
+b=int(b)
 print("result:%f"%(a+b))
 exit(0)
