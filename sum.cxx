c6c8ee63d8b0f621711d930d26ca34ba4e4d6e77
ryt <rytubuntulinux@sina.com>
2015-04-11 10:53:37 +0800

    commit at 2015-4-11 10:51:00

--- sum.cxx
+++ sum.cxx
@@ -1,12 +1,22 @@
 //A sum program
 #include<iostream>
+#include<cstdlib>    //double atof(char*)
 using namespace std;
 
 int main(void)
 {
-	float a,b;
-	cout<<"请输入要相加的两个数,并以空格分开:";
-	cin>>a>>b;
-	cout<<"结果:"<<a+b<<endl;
+	double m;
+	double n;
+	char a[10];
+	char b[10];
+	cout<<"Please input the first digit:";
+	cin>>a;
+	m=atof(a);
+	cout<<"Please input the second digit:";
+	cin>>b;
+	n=atof(b);
+	cout<<"The results is:"<<m+n<<endl;
+
+	
 	return 0;
 }
