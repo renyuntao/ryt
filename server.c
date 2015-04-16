154e07ff8c01b672f2f61621999c1661415d75a3
ryt <rytubuntulinux@sina.com>
2015-04-16 16:07:52 +0800

    commit at 2015-4-16

--- server.c
+++ server.c
@@ -1,42 +1,37 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<errno.h>
 #include<string.h>
-#include<sys/types.h>
+#include<stdlib.h>
+#include<sys/wait.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<netdb.h>
-#include<arpa/inet.h>
-#include<sys/wait.h>
 #include<signal.h>
 #define PORT "3490"
 #define BACKLOG 10
 
 void sigchld_handler(int s)
 {
-	while(waitpid(-1,NULL,WNOHANG)>0);
+	while(waitpid(-1,NULL,WNOHANG));  //waitpid() is defined in <sys/wait.h>
 }
-
-void *get_in_addr(struct sockaddr *sa)
-{
-	if(sa->sa_family==AF_INET)
-		return &(((struct sockaddr_in*)sa)->sin_addr);
+ void *get_in_addr(struct sockaddr *sa)    //struct sockaddr_in{} and struct sockaddr_in6{} is defined in <netinet/in.h>
+ {
+ 	if(sa->sa_family==AF_INET)               //struct sockaddr{} is defined in <sys/socket.h>
+	{
+		return &(((struct sockaddr_in*)sa)->sin_addr);    //macro AF_INET is defined in <netinet.h>
+	}
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
+ }
 
-
-int main(void)
-{
-	int sockfd,new_fd;
-	struct addrinfo hints,*servinfo,*p;
-	struct sockaddr_storage their_addr;
-	socklen_t sin_size;
-	struct sigaction sa;
+ int main(void)
+ {
+ 	int sockfd,new_fd;
+	struct addrinfo hints,*servinfo,*p;  //struct addrinfo{} is defined in <netdb.h>
+	struct sockaddr_storage their_addr;  //struct sockaddr_storage{} is defined in <netinet/in.h>
+	socklen_t sin_size;                  //socklen_t is defined in multiple header file,such as <sys/socket.h>
+	struct sigaction sa;                 //struct sigaction is defined in <signal.h>
 	int yes=1;
-	char s[INET6_ADDRSTRLEN];
+	char s[INET6_ADDRSTRLEN];            //macro INET6_ADDRSTRLEN is defined in <netinet/in.h>
 	int rv;
-
 
 	memset(&hints,0,sizeof(hints));
 	hints.ai_family=AF_UNSPEC;
@@ -45,10 +40,9 @@ int main(void)
 
 	if((rv=getaddrinfo(NULL,PORT,&hints,&servinfo))!=0)
 	{
-		fprintf(stderr,"getaddrinfo:%s\n",gai_strerror(rv));
+		fprintf(stderr,"getaddrinfo:%s\n",gai_strerror(rv));	
 		return 1;
 	}
-
 	for(p=servinfo;p!=NULL;p=p->ai_next)
 	{
 		if((sockfd=socket(p->ai_family,p->ai_socktype,p->ai_protocol))==-1)
@@ -65,10 +59,9 @@ int main(void)
 
 		if(bind(sockfd,p->ai_addr,p->ai_addrlen)==-1)
 		{
-			close(sockfd);
+			close(sockfd);     //close() is defined in <unistd.h>
 			perror("server:bind");
 		}
-
 		break;
 	}
 
@@ -96,36 +89,35 @@ int main(void)
 		exit(1);
 	}
 
-	printf("server:waiting for connections...\n");
+	printf("server:waiting for connections...");
 
 	while(1)
 	{
 		sin_size=sizeof(their_addr);
 		new_fd=accept(sockfd,(struct sockaddr*)&their_addr,&sin_size);
-
 		if(new_fd==-1)
 		{
-			perror("accept");
+			perror("accept");	
 			continue;
 		}
 
 		inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr*)&their_addr),s,sizeof(s));
-		printf("server:got connection from %s\n",s);
-
-		if(!fork())
-		{
-			close(sockfd);
 		
+		printf("server:get connnection from %s\n",s);
 
-			if(send(new_fd,"Hello,world!",13,0)==-1)
+		if(!fork())   //at child process
+		{
+			close(sockfd);	
+			if(send(new_fd,"Hello World!",13,0)==-1)
 			{
 				perror("send");
 			}
-
 			close(new_fd);
 			exit(0);
-		}	
+		}
 		close(new_fd);
-	}	
+	}
 	return 0;
-}
+ }
+
+
